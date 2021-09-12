5d4888c31b7276a81379a0f7d84b1999b8415167
dima-boom <78297357+dima-boom@users.noreply.github.com>
2021-09-12 09:07:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ try:
   def write_message(sender, message):
     authorize.method('messages.send', {'user_id': sender, 'message': message, "random_id": get_random_id()})
 
-  token = "a8b959ccc2bdecda28ed0ff7bcad846b6a3e248a37f288c3300af30dee895f41ad006ae00598e2e6a1bec"
+  token = "6bc780676c4a8bc3a16e76fa597c15c9367ad60568a8b23198ac83bfd3e1fdb1628b120565546540e9067"
   authorize = vk_api.VkApi(token=token)
   longpoll = VkLongPoll(authorize)
   for event in longpoll.listen():
@@ -23,20 +23,20 @@ try:
         # ПРОВЕРКА
         reseived_message = event.text.lower()
         sender = event.user_id
-        if sender == 671746933:
+        if sender == 664033661:
             if reseived_message == 'начать':
                 write_message(sender, "Работает!")
             elif reseived_message[0:4] == '/lii':
                 naz = text1(reseived_message)
                 ckok = text2(reseived_message)
                 requests.post('https://instagram777.ru/api', params={'key': '1356536ce1fe797f8e1ff1b092ef60fe',
-                                    'action': 'create', 'service': '122', 'quantity': int(ckok), 'link': str(naz)})
+                                    'action': 'create', 'service': '131', 'quantity': int(ckok), 'link': str(naz)})
                 write_message(sender, 'Всё ок')
             elif reseived_message[0:5] == '/poid':
                 naz = text1(reseived_message)
                 ckok = text2(reseived_message)
                 requests.post('https://instagram777.ru/api', params={'key': '1356536ce1fe797f8e1ff1b092ef60fe',
-                                    'action': 'create', 'service': '70', 'quantity': int(ckok), 'link': str(naz)})
+                                    'action': 'create', 'service': '177', 'quantity': int(ckok), 'link': str(naz)})
                 write_message(sender, 'Всё ок')
             elif reseived_message[0:5] == '/proc':
                 naz = text1(reseived_message)
